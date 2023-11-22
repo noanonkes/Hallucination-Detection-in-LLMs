4c4e53071bc3a981e8e9ae321b7b1f1cd08912bc
noanonkes <noanonkes@gmail.com>
2023-11-22 15:19:15 +0100

    added better documentation

--- dataloader.py
+++ dataloader.py
@@ -3,30 +3,76 @@ import torch
 import pandas as pd
 import numpy as np
 
+
 class SentenceLabelDataset(Dataset):
-    """Dataset."""
+    """
+    A PyTorch dataset to handle sentences and their associated labels from a CSV file.
+
+    This dataset loads sentences and their corresponding labels from a CSV file. 
+    It can handle various tasks such as classification, where labels are transformed into encoded vectors.
+
+    Args:
+        csv_file (str): Path to the CSV file containing sentences and labels.
+        limit (int, optional): Number of samples to use from the dataset (for debugging or limiting dataset size).
+
+    Attributes:
+        data (pandas.DataFrame): Loaded CSV data containing sentences and labels.
+
+    Methods:
+        rewrite_label(idx): Transforms a categorical label index into an encoded vector representation.
+    """
+
 
     def __init__(self, csv_file, limit=0):
         """
-        Arguments:
-            csv_file (string): Path to the csv file with labels.
-            limit (int): Number of samples to use (for debugging).
+        Initializes the SentenceLabelDataset with the provided CSV file path and optional limit for dataset size.
+
+        Args:
+            csv_file (str): Path to the CSV file containing sentences and labels.
+            limit (int, optional): Number of samples to use from the dataset (for debugging or limiting dataset size).
         """
         self.data = pd.read_csv(csv_file)
         if limit:
             self.data = self.data.head(limit)
 
+
     def rewrite_label(self, idx):
+        """
+        Transforms a categorical label index into an encoded vector representation.
+
+        Args:
+            idx (int): Categorical index representing a label.
+
+        Returns:
+            numpy.ndarray: Encoded vector representation of the label.
+        """
         cat2vec = np.array([[0,0,0],
                               [1,0,0],
                               [1,1,0],
                               [1,1,1]])
         return cat2vec[idx]
     
+
     def __len__(self):
+        """
+        Returns the total number of samples in the dataset.
+
+        Returns:
+            int: Number of samples in the dataset.
+        """
         return len(self.data)
 
+
     def __getitem__(self, idx):
+        """
+        Retrieves a specific sample from the dataset by index.
+
+        Args:
+            idx (int or slice): Index or slice indicating the sample(s) to retrieve.
+
+        Returns:
+            tuple: A tuple containing the sentence and its corresponding label.
+        """
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
