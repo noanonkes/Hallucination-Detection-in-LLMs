9a0ab868db30d8724f7600e0ccdd8645fc0d716b
noanonkes <noanonkes@gmail.com>
2023-11-22 14:47:52 +0100

    filter out too short MSMARCO answers

--- get_all_data.py
+++ get_all_data.py
@@ -28,48 +28,58 @@ elif len(answers_with_context) != 10000:
 	raise ValueError("Not enough 'with context' statements")
 
 df = pd.read_json("data/sampled_data.json", lines=True)
-with open("data/generated/full_train.txt", "a") as f:
-	for i, row in df.iterrows():
-		# answer
-		a = row['data']['paragraphs'][0]['qas'][0]['answers'][0]['text']		
-		# write the true answer
-		f.write(a + "\t" + "[1,1,1]" + "\n")
-		
-		# append the statements with no context
-		for j, answer in enumerate(answers_no_context[i * 5: i * 5 + 5]):
-			# case it did print "Answer: ..."
-			if "Answer" in answer:
-				# strip the "Answer x: " part
-				if j == 0:
-					# should be true without context
-					f.write(answer[10:] + "\t" + "[1,0,0]" + "\n")
-				else:
-					# false
-					f.write(answer[10:] + "\t" + "[0,0,0]" + "\n")
-			# case where it didn't have "Answer:..." format
-			else:
-				if j == 0:
-					# should be true without context
-					f.write(answer + "\t" + "[1,0,0]" + "\n")
-				else:
-					# false
-					f.write(answer + "\t" + "[0,0,0]" + "\n")
-
-		# append the statements with context
-		for j, answer in enumerate(answers_with_context[i * 5: i * 5 + 5]):
+data_dict = {"sentence": [], "label": []}
+for i, row in df.iterrows():
+	# answer
+	a = row['data']['paragraphs'][0]['qas'][0]['answers'][0]['text']		
+	# write the true answer
+	data_dict["sentence"].append(a)
+	data_dict["label"].append(3)
+	
+	# append the statements with no context
+	for j, answer in enumerate(answers_no_context[i * 5: i * 5 + 5]):
+		# case it did print "Answer: ..."
+		if "Answer" in answer:
 			# strip the "Answer x: " part
-			# case it did print "Answer: ..."
-			if "Answer" in answer:
-				if j == 0:
-					f.write(answer[10:] + "\t" + "[1,1,0]" + "\n")
-				else:
-					f.write(answer[10:] + "\t" + "[0,0,0]" + "\n")
-			# case where it didn't have "Answer:..." format
+			if j == 0:
+				# should be true without context
+				data_dict["sentence"].append(answer[10:])
+				data_dict["label"].append(1)
 			else:
-				if j == 0:
-					f.write(answer + "\t" + "[1,1,0]" + "\n")
-				else:
-					f.write(answer + "\t" + "[0,0,0]" + "\n")
+				# false
+				data_dict["sentence"].append(answer[10:])
+				data_dict["label"].append(0)
+		# case where it didn't have "Answer:..." format
+		else:
+			if j == 0:
+				# should be true without context
+				data_dict["sentence"].append(answer)
+				data_dict["label"].append(1)
+			else:
+				# false
+				data_dict["sentence"].append(answer)
+				data_dict["label"].append(0)
 
-df_full = pd.read_csv("data/generated/full_train.txt", sep="\t", header=None)
+	# append the statements with context
+	for j, answer in enumerate(answers_with_context[i * 5: i * 5 + 5]):
+		# strip the "Answer x: " part
+		# case it did print "Answer: ..."
+		if "Answer" in answer:
+			if j == 0:
+				data_dict["sentence"].append(answer[10:])
+				data_dict["label"].append(2)
+			else:
+				data_dict["sentence"].append(answer[10:])
+				data_dict["label"].append(0)
+		# case where it didn't have "Answer:..." format
+		else:
+			if j == 0:
+				data_dict["sentence"].append(answer)
+				data_dict["label"].append(2)
+			else:
+				data_dict["sentence"].append(answer)
+				data_dict["label"].append(0)
+
+df_full = pd.DataFrame(data_dict)
+df_full.to_csv("data/generated/full_train.csv", index=False)
 print(df_full.head(10))
