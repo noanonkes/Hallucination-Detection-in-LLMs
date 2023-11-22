db146809fea7417cf06588da2ac85bbd657bb85e
noanonkes <noanonkes@gmail.com>
2023-11-22 15:19:08 +0100

    threshold instead of top_k in getting edges

--- graph_utils.py
+++ graph_utils.py
@@ -3,34 +3,54 @@ import numpy as np
 import sklearn.metrics
 
 def get_embeddings(model, tokenizer, dataloader, device):
-    """Expects batch size of dataloader to be 1!"""
-    embeddings = []
-    for input, _ in dataloader:
-        encoded_input = tokenizer(input, return_tensors='pt').to(device) # padding=True, truncation=True ???
+    """
+    Expects batch size of dataloader to be 1.
 
-        # Forward pass through the model to get embeddings
+    Given a model and tokenizer, maps all the sentences
+    in the dataset to the givne model embedding.
+    """
+    embeddings = []
+    for text, _ in dataloader:
+        # manually add the CLS token
+        encoded_input = tokenizer("[CLS] " + text, return_tensors='pt').to(device)
+
+        # forward pass through the model to get embeddings
         with torch.no_grad():
             output = model(**encoded_input)
 
-        # Extract the embeddings
+        # extract the CLS embedding
         cls_embedding = output.last_hidden_state[:, 0, :]
         embeddings.append(cls_embedding[0])
     return torch.stack(embeddings)
 
 def get_labels(dataloader):
-    """Expects batch size of dataloader to be 1!"""
+    """
+    Expects batch size of dataloader to be 1.
+
+    Collects all the labels of the dataloader.
+    """
     labels = []
     for _, label in dataloader:
         labels.append(label[0])
     return torch.stack(labels)
 
-def get_edge_index(node_features, top_k=5):
-    """For now, each node also connects to itself."""
+def get_edge_index(node_features, threshold=.7):
+    """
+    If the cosine similarity between two node features is greater than 
+    the threshold, they will be connected via an edge.
+    """
     node_features = node_features.cpu().detach().numpy()
+
+    # calculate the cosine similarity between each node pair
     distances = sklearn.metrics.pairwise.cosine_similarity(node_features)
-    indices = np.argsort(distances, axis=1)[:, -top_k:]
+
+    # do not want to connect node to itself, inplace operation
+    np.fill_diagonal(distances, -1.)
+
     edge_index = []
-    for node, idxs in enumerate(indices):
-        for idx in idxs:
-            edge_index.append(torch.tensor([node, idx]))
+    for node_i, distances_i in enumerate(distances):
+        for node_j, distance_ij in enumerate(distances_i):
+            if distance_ij >= threshold:
+                edge_index.append(torch.tensor([node_i, node_j]))
+
     return torch.stack(edge_index)
