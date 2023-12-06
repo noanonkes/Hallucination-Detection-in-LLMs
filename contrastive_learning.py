5fac8fdaeb20fe390122ac0ee16ebaf57c8a6428
noanonkes <noanonkes@gmail.com>
2023-12-06 17:01:47 +0100

    contrastive loss implemented + loop best config

--- contrastive_learning.py
+++ contrastive_learning.py
@@ -4,6 +4,34 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import graph_utils
 from os.path import join as path_join
+from itertools import product
+
+class SimCLR(nn.Module):
+    """
+    Based on code from Deep Learning 1 tutorial by Phillip Lippe.
+    https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial17/SimCLR.html
+    """
+    def __init__(self, device, temperature=0.07):
+        super(SimCLR, self).__init__()
+        self.temperature = temperature
+        self.device = device
+
+    def forward(self, feats, labels):
+        # Calculate cosine similarity
+        cos_sim = F.cosine_similarity(feats[:,None,:], feats[None,:,:], dim=-1)
+        # Mask out cosine similarity to itself
+        self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=self.device)
+        cos_sim.masked_fill_(self_mask, 9e-15)
+        
+        # Find positive example -> where the labels are equal
+        pos_mask = torch.eq(labels, labels.T).to(self.device)
+
+        # InfoNCE loss
+        cos_sim = cos_sim / self.temperature
+        nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+        nll = nll.mean()
+
+        return nll
 
 class SimpleDataset(Dataset):
     def __init__(self, X, y):
@@ -15,45 +43,44 @@ class SimpleDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
-    
-class SelfSupervisedContrastiveLoss(nn.Module):
-    def __init__(self, device, temperature=0.07,
-                 base_temperature=0.07):
-        super(SelfSupervisedContrastiveLoss, self).__init__()
-        self.temperature = temperature
-        self.device = device
-        self.base_temperature = base_temperature
 
-    def forward(self, features, labels):
 
-        labels = labels.contiguous().view(-1, 1)
-        mask = torch.eq(labels, labels.T).float().to(self.device)
-        anchor_dot_contrast = torch.div(torch.matmul(features, features.T), self.temperature)
+def train_loop(dataloader, model,loss_func, optimizer, device):
+    model.train()
+    epoch_loss = 0.
+    for j, (embeddings, labels) in enumerate(dataloader):
+        optimizer.zero_grad()
 
-        # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=0, keepdim=True)
-        logits = anchor_dot_contrast - logits_max.detach()
+        embeddings, labels = embeddings.to(device), labels.to(device)
 
-        # mask out self-contrast cases
-        mask.fill_diagonal_(0.)
-        # mask[j] = 0
+        labels = torch.sum(labels, axis=1)
 
-        # compute log_prob
-        exp_logits = torch.exp(logits) * (1. - mask)
-        # print("1", exp_logits)
+        feats = model(embeddings)
+        train_loss = loss_func(feats, labels)
 
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdims=True))
-        # print("2", log_prob)
-        # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-        # print("3", mean_log_prob_pos)
+        train_loss.backward()
+        optimizer.step()
+        
+        epoch_loss += train_loss
 
-        # loss
-        loss = mean_log_prob_pos
-        loss = loss.mean()
+    return epoch_loss.item() / j
 
-        return loss
 
+def val_loop(dataloader, model, loss_func, device):
+    model.eval()
+    total_loss = 0.
+    with torch.no_grad():
+        for j, (embeddings, labels) in enumerate(dataloader):
+            embeddings, labels = embeddings.to(device), labels.to(device)
+
+            labels = torch.sum(labels, axis=1)
+
+            with torch.no_grad():
+                feats = model(embeddings)
+                val_loss = loss_func(feats, labels)
+                total_loss += val_loss
+
+    return total_loss.item() / j
 
 
 if __name__ == "__main__":
@@ -69,11 +96,6 @@ if __name__ == "__main__":
                         help="Number of cores to use when loading the data")
     parser.add_argument('--epochs', type=int, default=100,
                         help='Number of epochs to train the model')
-    parser.add_argument('--optimizer', type=str, default="Adam",
-                        choices=["SGD", "Adam"],
-                        help='Which optimizer to use for training')
-    parser.add_argument('--learning-rate', type=float, default=1e-3,
-                        help='Learning rate for the optimizer')
     args = parser.parse_args()
 
     # for reproducibility
@@ -91,67 +113,70 @@ if __name__ == "__main__":
         device = torch.device('cpu')
 
     # load graph
-    graph = torch.load(args.path + "graph.pt", map_location=device)
+    graph = torch.load(path_join(args.path, "graph.pt"), map_location=device)
 
+    # create a simple dataset in order to easily batch from
     train_dataset = SimpleDataset(graph.x[graph.train_idx], graph.y[graph.train_idx])
     val_dataset = SimpleDataset(graph.x[graph.val_idx], graph.y[graph.val_idx])
     print("Train dataset size:", len(train_dataset))
     print("Validate dataset size:", len(val_dataset))
 
-    train_loader = DataLoader(train_dataset, shuffle=True, batch_size=128, drop_last=True)
-    val_loader = DataLoader(val_dataset, shuffle=True, batch_size=128, drop_last=True)
+    # contrastive loss function
+    loss_func = SimCLR(device=device)
 
-    linear = nn.Linear(768, 64)
-    linear.to(device)
-
-    # loss_func = ContrastiveLoss()
-    loss_func = SelfSupervisedContrastiveLoss(device=device, temperature=0.5)
-
-    optimizer = graph_utils.get_optimizer(args.optimizer, linear, args.learning_rate)
-
-    best_val = float("inf")
-    for i in range(args.epochs):
-        linear.train()
-        batch_loss = 0
-        n_batch = 0
-        optimizer.zero_grad()
-        for j, (features, labels) in enumerate(train_loader):
-            features, labels = features.to(device), labels.to(device)
-            labels = torch.sum(labels, axis=1)
-
-            out = linear(features)
-            train_loss = loss_func(out, labels)
-            
+    # find configuration that has lowest loss
+    nlayers = [1, 2] # number of layers
+    activations = [nn.LeakyReLU(), nn.ReLU(), None]
+    optimizers = ["SGD", "Adam"]
+    learning_rates = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+    batch_sizes = [32, 64, 128, 256]
+    combinations = product(nlayers, activations,
+                           optimizers, learning_rates, batch_sizes)
     
-            batch_loss += train_loss
-            n_batch += 1
+    for n, activation, optimizer, lr, batch_size in combinations:
+        # do not run if n layers = 1 -> no activation function
+        if n == 1 and activation is not None:
+            continue
+        
+        # for every new configuration, empty cache
+        torch.cuda.empty_cache()
+        
+        # we also want to test different batch sizes
+        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, drop_last=True)
+        val_loader = DataLoader(val_dataset, shuffle=True, batch_size=batch_size, drop_last=True)
 
-        batch_loss.backward()
-        optimizer.step()
+        if activation is not None:
+            act_name = activation.__name__
+        else:
+            act_name = None
 
-        print(f'Epoch: {i}\n\ttrain loss: {batch_loss.item() / n_batch}')
+        config = f"n_{n}_act_{act_name}_opt_{optimizer}_lr_{lr}_bs_{batch_size}"
+        print(f"Configuration:\n\t{config}")
 
-        linear.eval()
-        batch_loss = 0
-        n_batch = 0
-        for j, (features, labels) in enumerate(val_loader):
-            features, labels = features.to(device), labels.to(device)
+        # create model according to the amount of layers and activation function
+        layers = [nn.Linear(768, 768)]
+        if activation is not None and n > 1:
+            layers.append(activation)
+        if n > 1:
+            layers.append(nn.Linear(768, 768))
 
-            labels = torch.sum(labels, axis=1)
+        model = nn.Sequential(*layers)
+        model.to(device)
 
-            with torch.no_grad():
-                out = linear(features)
-                val_loss = loss_func(out, labels)
+        optimizer = graph_utils.get_optimizer(optimizer, model, lr)
+        
+        best_val = float("inf")
+        for i in range(args.epochs):
+            train_loss = train_loop(train_loader, model, loss_func, optimizer, device)
+            val_loss = val_loop(val_loader, model, loss_func, device)
 
-                batch_loss += val_loss.item()
-                n_batch += 1
+            if best_val > val_loss:
+                best_val = val_loss
+                best_train = train_loss
+                best_i = i
+                save = {
+                'state_dict': model.state_dict(),
+                }
 
-
-        if best_val > batch_loss / n_batch:
-            best_val = batch_loss / n_batch
-            best_i = i
-            save = {
-            'state_dict': linear.state_dict(),
-            }
-
-    torch.save(save, path_join(args.output_dir, f"linear_{best_i}.pt"))
+        print(f'\tBest epoch: {best_i}\n\ttrain loss: {train_loss}\n\tval loss: {val_loss}')
+        torch.save(save, path_join(args.output_dir, f"{config}_{best_i}.pt"))                 
